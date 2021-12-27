1bab69606dabe4b7d0f8073a4d718ef7d393626b
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2021-12-27 08:58:24 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-ls -a
-aws s3 sync build/terraform-lambda.zip s3://$S3_BUCKET/$S3_BUCKET_KEY --delete
-aws lambda update-function-code --function-name $F1 --s3-bucket $S3_BUCKET --s3-key $S3_BUCKET_KEY
-aws lambda update-function-code --function-name $F2 --s3-bucket $S3_BUCKET --s3-key $S3_BUCKET_KEY
+aws s3 sync build/terraform-lambda.zip s3://prod-terraform-lambda-artifact-store-lwsrxw/terraform-lambda.zip --delete
+aws lambda update-function-code --function-name $F1 --s3-bucket prod-terraform-lambda-artifact-store-lwsrxw --s3-key terraform-lambda.zip
+aws lambda update-function-code --function-name $F2 --s3-bucket prod-terraform-lambda-artifact-store-lwsrxw --s3-key terraform-lambda.zip

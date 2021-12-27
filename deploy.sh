4b1743ac7d293c9aecd70169e7647c762576d569
Gerrard Ezeugwa <ezeugwagerrard@gmail.com>
2021-12-27 09:15:04 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-aws s3 sync build/terraform-lambda.zip s3://prod-terraform-lambda-artifact-store-lwsrxw/terraform-lambda.zip --delete
+aws s3 cp ./build/terraform-lambda.zip s3://prod-terraform-lambda-artifact-store-lwsrxw/terraform-lambda.zip
 aws lambda update-function-code --function-name $F1 --s3-bucket prod-terraform-lambda-artifact-store-lwsrxw --s3-key terraform-lambda.zip
 aws lambda update-function-code --function-name $F2 --s3-bucket prod-terraform-lambda-artifact-store-lwsrxw --s3-key terraform-lambda.zip
